c98bd4a28dd4310d7a1c2261bd018261c83d441c
Ian Dzingira T <iantdzingira@gmail.com>
2025-12-11 13:32:57 +0200

    Initial Commit

---
+++ Hackathon App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hackathon App
+//
+//  Created by Ian. T. Dzingira on 11/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hackathon App/Hackathon_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon_AppApp.swift
+//  Hackathon App
+//
+//  Created by Ian. T. Dzingira on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
